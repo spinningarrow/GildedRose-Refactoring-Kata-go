6507cb0f5514aa4dac5e93419a8ba7bcd4d7c966
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2019-08-14 16:32:47 +0200

    Use nicer for loop

--- gilded-rose.go
+++ gilded-rose.go
@@ -12,53 +12,51 @@ const (
 )
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != AgedBrie && items[i].name != BackstagePasses {
-			if items[i].quality > 0 {
-				if items[i].name != Sulfuras {
-					items[i].quality = items[i].quality - 1
+	for _, item := range items {
+		if item.name != AgedBrie && item.name != BackstagePasses {
+			if item.quality > 0 {
+				if item.name != Sulfuras {
+					item.quality = item.quality - 1
 				}
 			}
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == BackstagePasses {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+			if item.quality < 50 {
+				item.quality = item.quality + 1
+				if item.name == BackstagePasses {
+					if item.sellIn < 11 {
+						if item.quality < 50 {
+							item.quality = item.quality + 1
 						}
 					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+					if item.sellIn < 6 {
+						if item.quality < 50 {
+							item.quality = item.quality + 1
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].name != Sulfuras {
-			items[i].sellIn = items[i].sellIn - 1
+		if item.name != Sulfuras {
+			item.sellIn = item.sellIn - 1
 		}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != AgedBrie {
-				if items[i].name != BackstagePasses {
-					if items[i].quality > 0 {
-						if items[i].name != Sulfuras {
-							items[i].quality = items[i].quality - 1
+		if item.sellIn < 0 {
+			if item.name != AgedBrie {
+				if item.name != BackstagePasses {
+					if item.quality > 0 {
+						if item.name != Sulfuras {
+							item.quality = item.quality - 1
 						}
 					}
 				} else {
-					items[i].quality = items[i].quality - items[i].quality
+					item.quality = item.quality - item.quality
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.quality < 50 {
+					item.quality = item.quality + 1
 				}
 			}
 		}
 	}
-
 }
