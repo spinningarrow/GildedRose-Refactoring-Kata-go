9e89457cb97ecf9a1081889703b43bdeb74a93d8
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2019-08-14 16:34:06 +0200

    Merge some if conditions

--- gilded-rose.go
+++ gilded-rose.go
@@ -22,30 +22,20 @@ func updateQuality(item *Item) {
 	if item.name == BackstagePasses {
 		updateBackstagePassesQuality(item)
 	} else if item.name != AgedBrie && item.name != BackstagePasses {
-		if item.quality > 0 {
-			if item.name != Sulfuras {
-				item.quality = item.quality - 1
-			}
+		if item.name != Sulfuras && item.quality > 0 {
+			item.quality = item.quality - 1
 		}
-	} else {
-		if item.quality < 50 {
-			item.quality = item.quality + 1
-		}
+	} else if item.quality < 50 {
+		item.quality = item.quality + 1
 	}
 
 	if item.sellIn < 0 {
 		if item.name != AgedBrie {
-			if item.name != BackstagePasses {
-				if item.quality > 0 {
-					if item.name != Sulfuras {
-						item.quality = item.quality - 1
-					}
-				}
+			if item.name != BackstagePasses && item.quality > 0 && item.name != Sulfuras {
+				item.quality = item.quality - 1
 			}
-		} else {
-			if item.quality < 50 {
-				item.quality = item.quality + 1
-			}
+		} else if item.quality < 50 {
+			item.quality = item.quality + 1
 		}
 	}
 
@@ -54,15 +44,11 @@ func updateQuality(item *Item) {
 func updateBackstagePassesQuality(item *Item) {
 	if item.quality < 50 {
 		item.quality = item.quality + 1
-		if item.sellIn < 11 {
-			if item.quality < 50 {
-				item.quality = item.quality + 1
-			}
+		if item.sellIn < 11 && item.quality < 50 {
+			item.quality = item.quality + 1
 		}
-		if item.sellIn < 6 {
-			if item.quality < 50 {
-				item.quality = item.quality + 1
-			}
+		if item.sellIn < 6 && item.quality < 50 {
+			item.quality = item.quality + 1
 		}
 	}
 
